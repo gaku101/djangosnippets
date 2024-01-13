6327dbe83a3b6f73b47b31c44da371b95e227034
takehiro <takehiro-r133@hotmail.co.jp>
2024-01-13 14:26:26 +0900

    urlのマッピング

--- snippets/tests.py
+++ snippets/tests.py
@@ -1,6 +1,6 @@
-from django.http import HttpRequest
 from django.test import TestCase
-from snippets.views import top
+from django.urls import resolve
+from snippets.views import top, snippet_new, snippet_edit, snippet_detail
 
 
 class TopPageViewTest(TestCase):
@@ -11,3 +11,21 @@ class TopPageViewTest(TestCase):
     def test_top_returns_expected_content(self):
         response = self.client.get("/")
         self.assertEqual(response.content, b"Hello World")
+
+
+class CreateSnippetTest(TestCase):
+    def test_should_resulve_snippet_new(self):
+        found = resolve("/snippets/new/")
+        self.assertEqual(snippet_new, found.func)
+
+
+class SnippetDetailTest(TestCase):
+    def test_should_resolve_snippet_detail(self):
+        found = resolve("/snippets/1/")
+        self.assertEqual(snippet_detail, found.func)
+
+
+class EditSnippetTest(TestCase):
+    def test_should_resolve_snippet_edit(self):
+        found = resolve("/snippets/1/edit/")
+        self.assertEqual(snippet_edit, found.func)
